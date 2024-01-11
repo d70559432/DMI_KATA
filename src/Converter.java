fd8aac2071e75356a8b6bbe8bafc031b3127aab4
DMI220483 <dmi220483@gmail.com>
2024-01-11 17:38:07 +0300

    Test

--- src/Converter.java
+++ src/Converter.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Converter {
+class Converter {
+    static String[] romanArray = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI",
+            "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV",
+            "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI",
+            "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII",
+            "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII",
+            "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV",
+            "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV",
+            "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII",
+            "XCVIII", "XCIX", "C"};
+
+    public static boolean isRom(String val) {
+        for (int i = 0; i < romanArray.length; i++) {
+            if (val.equals(romanArray[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int convertToArab(String roman) {
+        for (int i = 0; i < romanArray.length; i++) {
+            if (roman.equals(romanArray[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static String convertToRom(int arabian) {
+        return romanArray[arabian];
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(info());
+    }
+    static String info() throws Exception{
+        System.out.println("Введите значение для расчета:");
+        Scanner scn = new Scanner(System.in);
+        String sc = scn.nextLine().trim(); // удаление не нужных пробелов в начале и в конце строки
+        return calc(sc);
+    }
+    public static String calc(String input) throws Exception {
+        int numberOne;
+        int numberTwo;
+        String arithmeticExpression;
+        String result;
+        boolean isRoman;
+        String[] aValues = input.split("[+\\-*/]");
+        if (aValues.length != 2) throw new Exception("Должно быть два операнда");
+        arithmeticExpression = detectOperation(input);
+        if (arithmeticExpression == null) throw new Exception("Неподдерживаемая математическая операция");
+        if (Converter.isRom(aValues[0]) && Converter.isRom(aValues[1])) {
+            numberOne = Converter.convertToArab(aValues[0]);
+            numberTwo = Converter.convertToArab(aValues[1]);
+            isRoman = true;
+        } else if (!Converter.isRom(aValues[0]) && !Converter.isRom(aValues[1])) {
+            numberOne = Integer.parseInt(aValues[0]);
+            numberTwo = Integer.parseInt(aValues[1]);
+            isRoman = false;
+        } else {
+            throw new Exception("Числа должны быть в одном формате");
+        }
+
+        if (numberOne > 10 || numberTwo > 10) {
+            throw new Exception("Числа должны быть от 1 до 10");
+        }
+
+        int arabian = calc(numberOne, numberTwo, arithmeticExpression);
+        if (isRoman) {
+            if (arabian <= 0) {
+                throw new Exception("Римское число должно быть больше нуля");
+            }
+            result = Converter.convertToRom(arabian);
+        } else {
+            result = String.valueOf(arabian);
+        }
+        return result;
+    }
+
+    static String detectOperation(String inputValue) {
+        if (inputValue.contains("+")) {
+            return "+";
+        } else if (inputValue.contains("-")) {
+            return "-";
+        } else if (inputValue.contains("*")) {
+            return "*";
+        } else if (inputValue.contains("/")) {
+            return "/";
+        } else {
+            return null;
         }
     }
+
+    static int calc(int valueOne, int valueTwo, String arithmeticExpression) {
+
+        if (arithmeticExpression.equals("+")) {
+            return valueOne + valueTwo;
+        } else if (arithmeticExpression.equals("-")) {
+            return valueOne - valueTwo;
+        } else if (arithmeticExpression.equals("*")) {
+            return valueOne * valueTwo;
+        } else {
+            return valueOne / valueTwo;
+        }
+    }
+
 }
+
+
+
+
